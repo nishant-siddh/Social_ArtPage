6802e4a977edf1bdb9b38c86945969dd6af6c72d
Nishant Siddh <94243692+nishant-siddh@users.noreply.github.com>
2023-11-02 10:30:37 +0530

    Update LeftSidebar.jsx

--- src/Components/LeftSidebar/LeftSidebar.jsx
+++ src/Components/LeftSidebar/LeftSidebar.jsx
@@ -4,7 +4,9 @@ import Sidebar from './Sidebar'
 const LeftSidebar = () => {
   return (
     <div className='flex flex-col gap-8'>
-      <div className='text-3xl h-20 bg-white rounded-lg flex items-center px-5 font-serif'>LOGO</div>
+      <div>
+        <div className='text-3xl h-20 bg-white rounded-lg flex items-center px-5 font-serif'>LOGO</div>
+      </div>
       <Sidebar />
     </div>
   )
